2bd8206bc6d7bfbc883b5f141ac4cb80433be172
Arkadiy Kukarkin <arkadiy@mine.nyc>
2016-12-19 14:32:19 -0500

    Remove app refs

--- truffle.js
+++ truffle.js
@@ -1,12 +1,5 @@
 module.exports = {
   build: {
-    "index.html": "index.html",
-    "app.js": [
-      "javascripts/app.js"
-    ],
-    "app.css": [
-      "stylesheets/app.css"
-    ]
   },
   rpc: {
     host: "localhost",
